d13cd16a88c9db5cb6a8074934238227a78a8ea8
esc358 <200478144@student.georgianc.on.ca>
2025-07-31 03:51:45 -0400

    created empty views activity for register, rename login.xml and refactor login.java

--- app/src/main/java/ca/georgiancollege/assignmenttwo_condeludena/Login.java
+++ app/src/main/java/ca/georgiancollege/assignmenttwo_condeludena/Login.java
@@ -8,18 +8,18 @@ package ca.georgiancollege.assignmenttwo_condeludena;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import ca.georgiancollege.assignmenttwo_condeludena.databinding.LoginBinding;
+import ca.georgiancollege.assignmenttwo_condeludena.databinding.ActivityLoginBinding;
 
 public class Login extends AppCompatActivity {
 
     //initialize binding for activity_main.xml
-    LoginBinding loginBinding;
+    ActivityLoginBinding activityLoginBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //assign binding to layout
-        loginBinding = LoginBinding.inflate(getLayoutInflater());
-        setContentView(loginBinding.getRoot());
+        activityLoginBinding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(activityLoginBinding.getRoot());
     }
 }
