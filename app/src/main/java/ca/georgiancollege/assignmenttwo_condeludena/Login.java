4209e6b841cd8fc7e0f711f7e3164cb03bd244fe
esc358 <200478144@student.georgianc.on.ca>
2025-07-31 05:23:36 -0400

    partially implemented firebase methods in login class

--- app/src/main/java/ca/georgiancollege/assignmenttwo_condeludena/Login.java
+++ app/src/main/java/ca/georgiancollege/assignmenttwo_condeludena/Login.java
@@ -10,13 +10,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 import ca.georgiancollege.assignmenttwo_condeludena.databinding.ActivityLoginBinding;
 
 public class Login extends AppCompatActivity {
 
     //initialize binding for activity_main.xml
     ActivityLoginBinding activityLoginBinding;
+    //initialize Firebase
+    private FirebaseAuth firebaseAuthentication;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +34,9 @@ public class Login extends AppCompatActivity {
         activityLoginBinding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(activityLoginBinding.getRoot());
 
+        //assign firebase to a new instance
+        firebaseAuthentication = FirebaseAuth.getInstance();
+
         //onclick for register button
         activityLoginBinding.buttonGoSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,8 +44,28 @@ public class Login extends AppCompatActivity {
                 //initialize new intent to Register
                 Intent intentRegister = new Intent(getApplicationContext(), Register.class);
                 startActivity(intentRegister);
+            }
+        });
+
+        //TODO:Complete method
+        //onclick for login button
+        activityLoginBinding.buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: Create new empty view activity, reuse views from movies recycler view
+            }
+        });
+    }
+    //TODO: Complete method
+    //void method to sign user
+    private void signUser(String email, String password){
+        //method from firebase to sign in with email and password
+        firebaseAuthentication.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
 
             }
         });
+
     }
 }
