bac1d0ea2470bf6c0dc551ea6d256b906a0a5169
esc358 <200478144@student.georgianc.on.ca>
2025-07-31 18:51:45 -0400

    implemented binding to AddEdit

--- app/src/main/java/ca/georgiancollege/assignmenttwo_condeludena/AddEdit.java
+++ app/src/main/java/ca/georgiancollege/assignmenttwo_condeludena/AddEdit.java
@@ -8,11 +8,18 @@ package ca.georgiancollege.assignmenttwo_condeludena;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import ca.georgiancollege.assignmenttwo_condeludena.databinding.ActivityAddEditBinding;
+
 public class AddEdit extends AppCompatActivity {
+    //initialize binding for activity_main.xml
+    ActivityAddEditBinding activityAddEditBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //assign binding to layout
+        activityAddEditBinding = ActivityAddEditBinding.inflate(getLayoutInflater());
+        setContentView(activityAddEditBinding.getRoot());
 
     }
 }
