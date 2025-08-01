5beeb312626eaf97301c4937f9b1fcb6b558369b
esc358 <200478144@student.georgianc.on.ca>
2025-08-01 02:25:17 -0400

    added Movie Model by reusing it from assigment one and targeting fields needed for this application

---
+++ app/src/main/java/ca/georgiancollege/assignmenttwo_condeludena/MovieModel.java
@@ -0,0 +1,46 @@
+/*
+ * Emilio Sebastian Conde Ludena
+ * 200478144
+ * August 1, 2025
+ * 2:20am
+ * From: https://github.com/esc358/assignmentOne_CondeLudena*/
+package ca.georgiancollege.assignmenttwo_condeludena;
+
+public class MovieModel {
+    private String title;
+    private String ratingValue;
+    private String year;
+    private String poster;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getRatingValue() {
+        return ratingValue;
+    }
+
+    public void setRatingValue(String ratingValue) {
+        this.ratingValue = ratingValue;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+}
