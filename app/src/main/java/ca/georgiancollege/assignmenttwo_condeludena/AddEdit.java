87ce0cabb76f334f8352edc9127f543e38957a19
esc358 <200478144@student.georgianc.on.ca>
2025-08-01 13:33:19 -0400

    instantiatenew  MovieViewModel use constructor from MovieModel to create a new instance of Movie and add it to Firebase with method from MovieViewModel

--- app/src/main/java/ca/georgiancollege/assignmenttwo_condeludena/AddEdit.java
+++ app/src/main/java/ca/georgiancollege/assignmenttwo_condeludena/AddEdit.java
@@ -10,12 +10,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import ca.georgiancollege.assignmenttwo_condeludena.databinding.ActivityAddEditBinding;
 
 public class AddEdit extends AppCompatActivity {
-    //initialize binding for activity_main.xml
+    //initialize binding for activity_main.xml and MovieViewModel
     ActivityAddEditBinding activityAddEditBinding;
+    private MovieViewModel movieViewModel;
 
 
     @Override
@@ -31,8 +33,9 @@ public class AddEdit extends AppCompatActivity {
         activityAddEditBinding.textMoviesListTitle.setText(titleText);
         activityAddEditBinding.addEditButton.setText(titleText);
 
+        //instantiate ViewModelProvider using MovieViewModel
+        movieViewModel = new ViewModelProvider(this).get(MovieViewModel.class);
 
-        //TODO: Logic for Add Edit
         //onclick for addEdit button
         activityAddEditBinding.addEditButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,10 +46,15 @@ public class AddEdit extends AppCompatActivity {
                 String movieRating = activityAddEditBinding.editRating.getText().toString();
                 String movieImageLink = activityAddEditBinding.editImageLink.getText().toString();
 
+                //use constructor of MovieModel to create a movie instance
+                MovieModel newMovie = new MovieModel(movieTitle, movieYear, movieRating, movieImageLink);
+                //use addMovie method to add to firebase
+                movieViewModel.addMovie(newMovie);
 
                 //initialize new intent to Movie Activity
                 Intent intentMovieActivity = new Intent(getApplicationContext(), MovieActivity.class);
                 startActivity(intentMovieActivity);
+                finish();
             }
         });
 
